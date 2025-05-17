c413c189dd420437dc3e3875c1b7cb567b239ce3
fa1zali <fa1zali@icloud.com>
2025-05-17 13:51:03 +0530

    Added views and url's

--- food/views.py
+++ food/views.py
@@ -5,7 +5,14 @@ from .models import Item
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    context = {
+        'item_list': item_list
+    }
+    return render(request, 'food/index.html', context)
 
-def item(request):
-    return HttpResponse("<h1>This is an item</h1>")
+def detail(request, item_id):
+    item = Item.objects.get(pk = item_id)
+    context = {
+        'item': item
+    }
+    return render(request, 'food/detail.html', context)
